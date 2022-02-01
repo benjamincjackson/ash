31ac32add0d072ddefbfe8dfc1c444d11158a03d
benjamincjackson <benjamincjackson@gmail.com>
2022-02-01 14:20:01 +0000

    lower memory for temporally unresolved things

--- pkg/epistasis/epistasis.go
+++ pkg/epistasis/epistasis.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math"
 	"runtime"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -21,25 +22,25 @@ type Result struct {
 
 // a struct containing info about one pair's consecutive mutations over the tree
 type Pair struct {
-	i     string                     // the name of the first site
-	j     string                     // the name of the second site
-	m_ij  int                        // number of branches with changes at BOTH sites i and j on
-	t_pi  [][]float64                // list of synonymous distances between pairs of nonsynonymous changes at sites i and j, one slice for each possible order
-	order []map[*tree.Edge][2]string // what is the order of the temporally unresolved mutations in this permutation
+	i     string                   // the name of the first site
+	j     string                   // the name of the second site
+	m_ij  int                      // number of branches with changes at BOTH sites i and j on
+	t_pi  []float64                // list of synonymous distances between pairs of nonsynonymous changes at sites i and j
+	order map[*tree.Edge][2]string // what is the order of the temporally unresolved mutations in this permutation
 	E_tau float64
 }
 
-func (p *Pair) add_m_ij() {
-	p.m_ij++
-}
+// func (p *Pair) add_m_ij() {
+// 	p.m_ij++
+// }
 
 func (p *Pair) set_m_ij(m_ij int) {
 	p.m_ij = m_ij
 }
 
 // n is the nth possible order, i is a value of t_pi
-func (p *Pair) append_t_pi(n int, i float64) {
-	p.t_pi[n] = append(p.t_pi[n], i)
+func (p *Pair) append_t_pi(i float64) {
+	p.t_pi = append(p.t_pi, i)
 }
 
 func (p *Pair) get_i() string {
@@ -51,35 +52,37 @@ func (p *Pair) get_j() string {
 }
 
 // n is the nth possible order
-func (p *Pair) get_order(n int) map[*tree.Edge][2]string {
-	return p.order[n]
+func (p *Pair) get_order() map[*tree.Edge][2]string {
+	return p.order
 }
 
-// 1 << m_ij is the same as 2^^m_ij
-func (p *Pair) set_order_length(m_ij int) {
-	if m_ij > 0 {
-		p.order = make([]map[*tree.Edge][2]string, 1<<m_ij, 1<<m_ij)
-		for i := 0; i < 1<<m_ij; i++ {
-			p.order[i] = make(map[*tree.Edge][2]string)
-		}
-	}
+// // 1 << m_ij is the same as 2^^m_ij
+// func (p *Pair) set_order_length(m_ij int) {
+// 	if m_ij > 0 {
+// 		p.order = make([]map[*tree.Edge][2]string, 1<<m_ij, 1<<m_ij)
+// 		for i := 0; i < 1<<m_ij; i++ {
+// 			p.order[i] = make(map[*tree.Edge][2]string)
+// 		}
+// 	}
+// }
+
+func (p *Pair) set_order(m map[*tree.Edge][2]string) {
+	p.order = m
 }
 
-func (p *Pair) set_order(n int, m map[*tree.Edge][2]string) {
-	p.order[n] = m
-}
-
-func (p *Pair) initialise_t_ij_slice(m_ij int) {
-	p.t_pi = make([][]float64, 1<<m_ij)
-	for i := 0; i < 1<<m_ij; i++ {
-		p.t_pi[i] = make([]float64, 0)
-	}
-}
+// func (p *Pair) initialise_t_ij_slice(m_ij int) {
+// 	p.t_pi = make([][]float64, 1<<m_ij)
+// 	for i := 0; i < 1<<m_ij; i++ {
+// 		p.t_pi[i] = make([]float64, 0)
+// 	}
+// }
 
 // the tree's branches are labelled with "syn=" for non-protein-changing nucleotide changes,
 // and "AA=" for amino acid changes
 // edge.SynLen is also set to the inferred synonymous branch length
 func Epistasis(t *tree.Tree, features []annotation.Region, threads int) {
+	// get_m_total(t)
+	// os.Exit(0)
 	// the mean synonymous distance between non-synonymous pairs:
 	tau := get_tau(t)
 	fmt.Println("tau is: " + strconv.FormatFloat(tau, 'f', 8, 64))
@@ -139,6 +142,45 @@ func Epistasis(t *tree.Tree, features []annotation.Region, threads int) {
 	// for i := range results {
 	// 	fmt.Println(results[i])
 	// }
+}
+
+// possible temporary function to summarise all the temporarily-unresolved mutation-branch combinations
+func get_m_total(t *tree.Tree) {
+	m := make(map[string]int)
+	for _, e := range t.Edges() {
+		// skip external branches
+		if e.Right().Tip() {
+			continue
+		}
+		AAs := e.Get_AA_residues()
+		for i := range AAs {
+			for j := i + 1; j < len(AAs); j++ {
+				p := []string{AAs[i], AAs[j]}
+				sort.Strings(p)
+				s := strings.Join(p, "_")
+				m[s]++
+			}
+		}
+	}
+
+	type simple struct {
+		name  string
+		value int
+	}
+
+	key_val_pair := make([]simple, 0)
+
+	for k, v := range m {
+		key_val_pair = append(key_val_pair, simple{k, v})
+	}
+
+	sort.Slice(key_val_pair, func(i, j int) bool {
+		return key_val_pair[i].value > key_val_pair[j].value
+	})
+
+	for _, kvp := range key_val_pair {
+		fmt.Println(kvp.name + " " + strconv.Itoa(kvp.value))
+	}
 }
 
 // following Kryazhimskiy, Sergey, et al. "Prevalence of epistasis in the evolution of influenza A surface proteins." PLoS genetics 7.2 (2011): e1001301,
@@ -204,8 +246,37 @@ func process_pairs(cPairIn chan Pair, cPairOut chan Result, t *tree.Tree, tau fl
 
 func process_one_pair(p Pair, t *tree.Tree, tau float64) Result {
 
-	get_set_temporally_unresolved(t, &p)
-	ij(t, &p)
+	i := p.get_i()
+	j := p.get_j()
+	m_ij, unresolved_edges := get_temporally_unresolved(t, &p)
+
+	p.set_m_ij(m_ij)
+
+	// if there are no unresolved edges for this pair, we can
+	// recur down the tree without too many cares in the world
+	if m_ij == 0 {
+		ij_recur(nil, t.Root(), &p, false, 0.0)
+	} else {
+		// otherwise, let's do the order 2^^n stuff in a loop to
+		// prevent an explosion of memory use
+		for k := 0; k < 1<<m_ij; k++ {
+			order := order(m_ij, k)
+			om := make(map[*tree.Edge][2]string)
+			for l := range unresolved_edges {
+				switch order[l] {
+				case 0:
+					om[unresolved_edges[l]] = [2]string{i, j}
+				case 1:
+					om[unresolved_edges[l]] = [2]string{j, i}
+				}
+			}
+			p.set_order(om)
+			ij_recur_unresolved(nil, t.Root(), &p, false, 0.0)
+		}
+	}
+
+	// get_set_temporally_unresolved(t, &p)
+	// ij(t, &p)
 	p.E_tau = calc_E_tau(&p, tau)
 
 	return Result{i: p.i, j: p.j, E_tau: p.E_tau}
@@ -281,44 +352,60 @@ func printResults(cResultsIn chan Result, cWriteDone chan bool) {
 // 	return pairs
 // }
 
-/*
-modified from: https://github.com/mxschmitt/golang-combinations/blob/master/combinations.go
+// /*
+// modified from: https://github.com/mxschmitt/golang-combinations/blob/master/combinations.go
 
-MIT License
+// MIT License
 
-Copyright (c) 2018 Max Schmitt
+// Copyright (c) 2018 Max Schmitt
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-*/
-func orders(n int) (subsets [][]int) {
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// */
+// func orders(n int) (subsets [][]int) {
+// 	if n == 0 {
+// 		return make([][]int, 0)
+// 	}
+// 	length := uint(n)
+
+// 	for subsetBits := 0; subsetBits < (1 << length); subsetBits++ {
+// 		subset := make([]int, n)
+
+// 		for object := uint(0); object < length; object++ {
+// 			// checks if object is contained in subset
+// 			// by checking if bit 'object' is set in subsetBits
+// 			if (subsetBits>>object)&1 == 1 {
+// 				// add object to subset
+// 				subset[object] = 1
+// 			}
+// 		}
+// 		// add subset to subsets
+// 		subsets = append(subsets, subset)
+// 	}
+// 	return subsets
+// }
+
+func order(n, m int) []int {
 	if n == 0 {
-		return make([][]int, 0)
+		return make([]int, 0)
 	}
 	length := uint(n)
+	subsetBits := uint(m)
+	order := make([]int, n)
 
-	for subsetBits := 0; subsetBits < (1 << length); subsetBits++ {
-		subset := make([]int, n)
-
-		for object := uint(0); object < length; object++ {
-			// checks if object is contained in subset
-			// by checking if bit 'object' is set in subsetBits
-			if (subsetBits>>object)&1 == 1 {
-				// add object to subset
-				subset[object] = 1
-			}
+	for object := uint(0); object < length; object++ {
+		if (subsetBits>>object)&1 == 1 {
+			order[object] = 1
 		}
-		// add subset to subsets
-		subsets = append(subsets, subset)
 	}
-	return subsets
+	return order
 }
 
-func get_set_temporally_unresolved(t *tree.Tree, p *Pair) {
+func get_temporally_unresolved(t *tree.Tree, p *Pair) (int, []*tree.Edge) {
 	var i_present bool
 	var j_present bool
 	var site string
@@ -352,25 +439,62 @@ func get_set_temporally_unresolved(t *tree.Tree, p *Pair) {
 		}
 	}
 
-	p.set_m_ij(m_ij)
-	p.set_order_length(m_ij)
-	p.initialise_t_ij_slice(m_ij)
-
-	os := orders(m_ij)
-	for n := range os {
-		om := make(map[*tree.Edge][2]string)
-		for m := range unresolved {
-			switch os[n][m] {
-			case 0:
-				om[unresolved[m]] = [2]string{i, j}
-			case 1:
-				om[unresolved[m]] = [2]string{j, i}
-			}
-
-		}
-		p.set_order(n, om)
-	}
+	return m_ij, unresolved
 }
+
+// func get_set_temporally_unresolved(t *tree.Tree, p *Pair) {
+// 	var i_present bool
+// 	var j_present bool
+// 	var site string
+
+// 	i := p.get_i()
+// 	j := p.get_j()
+
+// 	m_ij := 0
+
+// 	unresolved := make([]*tree.Edge, 0)
+
+// 	for _, e := range t.Edges() {
+// 		i_present = false
+// 		j_present = false
+// 		for _, comment := range e.GetComments() {
+// 			if strings.HasPrefix(comment, "syn=") {
+// 				continue
+// 			}
+// 			site = strings.Join(strings.Split(comment, ":")[0:2], ":")
+// 			if site == i {
+// 				i_present = true
+// 			}
+// 			if site == j {
+// 				j_present = true
+// 			}
+// 			if i_present && j_present {
+// 				m_ij++
+// 				unresolved = append(unresolved, e)
+// 				break
+// 			}
+// 		}
+// 	}
+
+// 	p.set_m_ij(m_ij)
+// 	p.set_order_length(m_ij)
+// 	p.initialise_t_ij_slice(m_ij)
+
+// 	os := orders(m_ij)
+// 	for n := range os {
+// 		om := make(map[*tree.Edge][2]string)
+// 		for m := range unresolved {
+// 			switch os[n][m] {
+// 			case 0:
+// 				om[unresolved[m]] = [2]string{i, j}
+// 			case 1:
+// 				om[unresolved[m]] = [2]string{j, i}
+// 			}
+
+// 		}
+// 		p.set_order(n, om)
+// 	}
+// }
 
 // for recording the synonymous distances between nonsynon substitutions
 type Tns struct {
@@ -474,17 +598,17 @@ func collect_distances(prevEdge *tree.Edge, curNode *tree.Node, tns *Tns, n int,
 
 // {AA=orf1ab:4387 AA=orf1ab:6485 0 []}
 
-func ij(t *tree.Tree, p *Pair) {
-	switch p.m_ij {
-	case 0:
-		ij_recur(nil, t.Root(), p, false, 0.0)
-	default:
-		for i := range p.order {
-			ij_recur_unresolved(nil, t.Root(), p, i, false, 0.0)
-		}
-	}
-	// fmt.Println(*p)
-}
+// func ij(t *tree.Tree, p *Pair) {
+// 	switch p.m_ij {
+// 	case 0:
+// 		ij_recur(nil, t.Root(), p, false, 0.0)
+// 	default:
+// 		for i := range p.order {
+// 			ij_recur_unresolved(nil, t.Root(), p, i, false, 0.0)
+// 		}
+// 	}
+// 	// fmt.Println(*p)
+// }
 
 func ij_recur(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, open bool, synDist float64) {
 
@@ -551,7 +675,7 @@ func ij_recur(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, open bool, sy
 			o = true
 			d = synDist + float64(edgeSynDist)
 		case open && !i_present && j_present: // 101
-			pair.append_t_pi(0, synDist+(float64(edgeSynDist)/2))
+			pair.append_t_pi(synDist + (float64(edgeSynDist) / 2))
 			o = false
 			d = 0.0
 		case open && i_present && !j_present: // 110
@@ -565,7 +689,7 @@ func ij_recur(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, open bool, sy
 	}
 }
 
-func ij_recur_unresolved(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, order int, open bool, synDist float64) {
+func ij_recur_unresolved(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, open bool, synDist float64) {
 
 	// base state: if we have reached a tip, we don't want to do anything (per the original paper -
 	// mutations on external edges are discounted)
@@ -626,7 +750,7 @@ func ij_recur_unresolved(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, or
 			o = true
 			d = synDist + float64(edgeSynDist)
 		case open && !i_present && j_present: // 101
-			pair.append_t_pi(order, synDist+(float64(edgeSynDist)/2))
+			pair.append_t_pi(synDist + (float64(edgeSynDist) / 2))
 			o = false
 			d = 0.0
 		case open && i_present && !j_present: // 110
@@ -635,11 +759,11 @@ func ij_recur_unresolved(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, or
 
 		case !open && i_present && j_present: // 011
 
-			first := pair.get_order(order)[e][0]
+			first := pair.get_order()[e][0]
 			// second := pair.get_order(order)[e][0]
 
 			if first == i { // xij
-				pair.append_t_pi(order, (float64(edgeSynDist) / 3))
+				pair.append_t_pi((float64(edgeSynDist) / 3))
 				o = false
 				d = 0.0
 			} else { // xji
@@ -648,15 +772,15 @@ func ij_recur_unresolved(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, or
 			}
 		case open && i_present && j_present: // 111
 
-			first := pair.get_order(order)[e][0]
+			first := pair.get_order()[e][0]
 			// second := pair.get_order(order)[e][0]
 
 			if first == i { // iij
-				pair.append_t_pi(order, (float64(edgeSynDist) / 3))
+				pair.append_t_pi((float64(edgeSynDist) / 3))
 				o = false
 				d = 0.0
 			} else { // iji
-				pair.append_t_pi(order, synDist+(float64(edgeSynDist)/3))
+				pair.append_t_pi(synDist + (float64(edgeSynDist) / 3))
 				o = true
 				d = float64(edgeSynDist) / 3
 			}
@@ -664,18 +788,28 @@ func ij_recur_unresolved(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, or
 
 		// func ij_recur(prevEdge *tree.Edge, curNode *tree.Node, pair *Pair, open [2]bool, synDist float64) {
 		// then we carry on recurring down the tree
-		ij_recur_unresolved(e, curNode.Neigh()[k], pair, order, o, d)
+		ij_recur_unresolved(e, curNode.Neigh()[k], pair, o, d)
 	}
 }
 
 // see equation (1) in Kryazhimskiy, Sergey, et al. "Prevalence of epistasis in the evolution of influenza A surface proteins." PLoS genetics 7.2 (2011): e1001301.
+// func calc_E_tau(p *Pair, tau float64) float64 {
+// 	d := 1 << p.m_ij
+// 	sum := 0.0
+// 	for i := range p.t_pi {
+// 		for j := range p.t_pi[i] {
+// 			sum += math.Exp((p.t_pi[i][j] * -1) / tau)
+// 		}
+// 	}
+// 	E_tau := sum / float64(d)
+// 	return E_tau
+// }
+
 func calc_E_tau(p *Pair, tau float64) float64 {
 	d := 1 << p.m_ij
 	sum := 0.0
 	for i := range p.t_pi {
-		for j := range p.t_pi[i] {
-			sum += math.Exp((p.t_pi[i][j] * -1) / tau)
-		}
+		sum += math.Exp((p.t_pi[i] * -1) / tau)
 	}
 	E_tau := sum / float64(d)
 	return E_tau
